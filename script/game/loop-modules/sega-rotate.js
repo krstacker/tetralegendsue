c9c945ff5aad805fa8f52c995759d9d19cec12b2
Sub2Ninah <70987554+krstacker@users.noreply.github.com>
2025-11-14 16:09:33 +0800

    Update sega-rotate.js

--- script/game/loop-modules/sega-rotate.js
+++ script/game/loop-modules/sega-rotate.js
@@ -2,9 +2,9 @@ import input from "../../input.js"
 
 export default function segaRotate(arg) {
   const piece = arg.piece
-  if (piece.y < 4 && piece.shape === "I") {
+  if (piece.y + 1 < 4 && piece.shape === "I") {
 	  return
-  } else if (piece.y < 2) {
+  } else if (piece.y + 1 < 2) {
 	  return
   }
   if (input.getGamePress("rotateLeft")) {
