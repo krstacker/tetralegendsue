e73658a404d1fe9676c4b61850e36ceb9f03352a
Sub2Ninah <70987554+krstacker@users.noreply.github.com>
2025-11-19 17:43:04 +0800

    Update stack.js

--- script/game/stack.js
+++ script/game/stack.js
@@ -995,6 +995,11 @@ export default class Stack extends GameModule {
         const yPos =
           y * cellSize + cellSize * buffer - cellSize * this.hiddenHeight
         img.height = cellSize
+		if (this.isHidden === false && this.isFading === false) {
+			ctx.fillStyle = "#000000"
+			ctx.fillRect(xPos, Math.floor(yPos), cellSize, cellSize)
+			ctx.globalCompositeOperation = "overlay"
+		}
         ctx.drawImage(img, xPos, Math.floor(yPos), cellSize, cellSize)
         ctx.globalCompositeOperation = "multiply"
         ctx.fillStyle = "#0003"
